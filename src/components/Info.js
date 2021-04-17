8088ff2d6b4b41df68b69f1469e51cc38364bceb
ferengitooth <ehitsamill@gmail.com>
2021-04-16 17:11:25 -0700

    added a bit of style to info.js

--- src/components/Info.js
+++ src/components/Info.js
@@ -2,16 +2,28 @@ import React from 'react'
 import styled from 'styled-components'
 import Character from './Character'
 
+const InfoCard = styled.div`
+    border: 1px solid black;
+    background-color:black;
+    color:white;
+    margin:1rem;
+    padding:2%;
+
+`
+
+
+
+
 const Info = props => {
     
     const {stats} = props
 
     return (
-        <div>
+        <InfoCard>
             <p>Name:{stats.name}</p>
             <p>Height:{stats.height}</p>
             <p>Mass:{stats.mass}</p>
-        </div>
+        </InfoCard>
     )
 }
 
